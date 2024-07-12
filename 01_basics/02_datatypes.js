47306638717f4b40f55fdf3128f3d6b1dff0fb68
student-prashan <164781773+student-prashan@users.noreply.github.com>
2024-07-12 19:25:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as new version 
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3+
+    3)//code readability should be high
+
+    console.log("prashant");
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined
+    //symbol => unique
+
+    //object
+
+    console.log(typeof string);
+    console.log(typeof BigInt);
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); //object
