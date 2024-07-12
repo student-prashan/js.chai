4ebfc84068d810c84269143fce990eb4996103ea
student-prashan <164781773+student-prashan@users.noreply.github.com>
2024-07-12 19:22:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const acc_id =6969
+let acc_email="dev@google.com"
+var acc_password="38945"
+acc_city="rewari"
+let acc_state;
+
+//acc_id =5; NOT ALLOWED 
+
+acc_email="hc@gv.com"
+acc_password="9696"
+acc_city="sonipat"
+
+// PREFER NOT TO USE VAR BCZ OF ISSUE IN BLOCK AND FUNCTIONAL SCOPE 
+ 
+
+console.table([acc_id,acc_email,acc_password,acc_state,acc_city]);
