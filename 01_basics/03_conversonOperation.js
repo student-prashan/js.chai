8114c7e0fda1624c7f668b09437162515b5ab2a0
student-prashan <164781773+student-prashan@users.noreply.github.com>
2024-07-13 18:18:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversonOperation.js
@@ -0,0 +1,21 @@
+let score = "77k"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false=>0
+
+let islogin = "";
+
+let booleanislogin= String(islogin)
+console.log(booleanislogin);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
