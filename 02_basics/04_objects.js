1726c21c305fd56c08c767c76633763d1b623cf4
student-prashan <164781773+student-prashan@users.noreply.github.com>
2024-07-23 11:33:07 +0000

    OBJECTSDS

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,12 @@
+const course ={
+    cname:"js in hindi ",
+    price: "999",
+    cinstructor:"rishab"
+}
+
+// course.instructor
+
+const {cinstructor: instructor}=course
+
+//console.log(cinstructor);
+console.log(instructor);
