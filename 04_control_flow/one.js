b5a726a151acd2da0f63a0178a63a5f5bf7c111f
student-prashan <164781773+student-prashan@users.noreply.github.com>
2024-07-27 14:49:49 +0000

    onejs

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+// IF
+
+const isuserloggedin=true;
+const temp= 41;
+
+if(temp === 40){
+    console.log("less than 50");
+}else{
+    console.log("temp is greater than 50");
+}
+
+console.log("executed");
+
+// ===, !==
+
+const score =200;
+
+if (score>100){
+    const power ="fly"
+    console.log(`user power: ${power}`);
+}
+console.log(`user power: ${power}`)
