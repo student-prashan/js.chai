3e742225cdb0febfdea82e9e5d3a58d170c50444
student-prashan <164781773+student-prashan@users.noreply.github.com>
2024-07-27 14:50:14 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,24 @@
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+
+const month=8
+
+switch (month) {
+    case 1:
+                console.log("may");
+        break;
+    
+        case 2:
+                console.log("june");
+        break;
+
+    default:
+        console.log("default value");
+        break;
+}
