3287cf4d47f605f3566cdc3a452881089fcdbb7a
student-prashan <164781773+student-prashan@users.noreply.github.com>
2024-08-13 14:23:53 +0000

    5.js

---
+++ 05_iterations/5.js
@@ -0,0 +1,17 @@
+const mynumers=[1,2,3,4,5,6,7,8,9,10]
+
+const newnums= mynumers.map((num)=> num +9);
+//console.log(newnums);
+
+mynumers.forEach(element => {
+    //console.log(element+10);
+    
+});
+
+const newwnums= mynumers.map((num)=> num*10)
+                .map((num)=> num+1)
+                .filter((num)=>num>=40)
+
+                console.log(newwnums);
+                
+    
